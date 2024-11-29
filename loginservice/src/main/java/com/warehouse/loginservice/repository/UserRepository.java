017a351815da44d4d182ae4fa401a05e2be0dc62
Duc Trong <trong.nguyen.cit20@eiu.edu.vn>
2024-11-29 22:45:35 +0700

    Create User Entity, Use User Detail and Create JWT Service

---
+++ loginservice/src/main/java/com/warehouse/loginservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.warehouse.loginservice.repository;
+
+import com.warehouse.loginservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ loginservice/src/main/java/com/warehouse/loginservice/service/JWTService.java
@@ -0,0 +1,4 @@
+package com.warehouse.loginservice.service;
+
+public interface JWTService {
+}

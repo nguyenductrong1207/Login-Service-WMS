985bb3bcad16307ea46a7c20eb7c598b14f849d2
Duc Trong <trong.nguyen.cit20@eiu.edu.vn>
2025-02-16 20:46:35 +0700

    add application-test.properties file

--- loginservice/src/test/java/com/warehouse/loginservice/LoginserviceApplicationTests.java
+++ loginservice/src/test/java/com/warehouse/loginservice/LoginserviceApplicationTests.java
@@ -1,12 +1,16 @@
 package com.warehouse.loginservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class LoginserviceApplicationTests {
+//@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class LoginserviceApplicationTests {
 
 	@Test
 	void contextLoads() {

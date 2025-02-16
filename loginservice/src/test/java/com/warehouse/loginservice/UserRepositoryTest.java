428c2f7652cf7255a38a49fbd1c9330712f70d8e
Duc Trong <trong.nguyen.cit20@eiu.edu.vn>
2025-02-16 21:32:51 +0700

    repair for deploying

---
+++ loginservice/src/test/java/com/warehouse/loginservice/UserRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.warehouse.loginservice;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class UserRepositoryTest {
+}
